c0fef83e3119b2f0c5758478ea2c4ea23a568123
Alex Pochebut <alexpochebut96@gmail.com>
2023-02-19 12:37:47 +0300

    feat(app): add new modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,36 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
-import { AuthModule } from './auth/auth.module'
-import { UserModule } from './user/user.module'
-import { PrismaModule } from './prisma/prisma.module'
-import { ConfigModule } from '@nestjs/config'
-import { APP_GUARD } from '@nestjs/core'
-import { FingerprintMiddleware } from './global/middlewares'
-import { AtGuard, FpGuard } from './auth/guard'
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common'
+import {AuthModule} from './auth/auth.module'
+import {UserModule} from './user/user.module'
+import {PrismaModule} from './prisma/prisma.module'
+import {ConfigModule} from '@nestjs/config'
+import {APP_GUARD} from '@nestjs/core'
+import {FingerprintMiddleware} from './global/middlewares'
+import {AtGuard, FpGuard} from './auth/guard'
+import {CardsModule} from './cards/cards.module';
+import {ModulesModule} from './modules/modules.module';
+import {LearnSessionsModule} from './learn-sessions/learn-sessions.module';
+import {CurrentLearnSessionModule} from './current-learn-session/current-learn-session.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    UserModule,
-    PrismaModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-  ],
-  providers: [
-    { provide: APP_GUARD, useClass: AtGuard },
-    { provide: APP_GUARD, useClass: FpGuard },
-  ],
+    imports: [
+        AuthModule,
+        UserModule,
+        PrismaModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        CardsModule,
+        ModulesModule,
+        LearnSessionsModule,
+        CurrentLearnSessionModule,
+    ],
+    providers: [
+        {provide: APP_GUARD, useClass: AtGuard},
+        {provide: APP_GUARD, useClass: FpGuard},
+    ],
 })
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer): any {
-    consumer.apply(FingerprintMiddleware).forRoutes('*')
-  }
+    configure(consumer: MiddlewareConsumer): any {
+        consumer.apply(FingerprintMiddleware).forRoutes('*')
+    }
 }
